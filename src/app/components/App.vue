ef90a962ee1ae57afeefa06045d8c13f30065051
yo-huaca95 <yo.huaca@udla.edu.co>
2021-10-02 23:40:44 -0500

    actualización 2

---
+++ src/app/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+ <div>
+    <nav class="navbar navbar-ligth bg-linght">
+      <a href="/" class="navbar-brand">MEVN Stack</a>
+    </nav>
+    <div class="container">
+      <div class="row">
+          <div class="col-md-5">
+              <div class="card-body">
+                  <form>
+                      <div class="form-group">
+                          <input type="text" placeholder="Tarea" class="form-control">
+                      </div>
+                      <div class="form-group">
+                          <textarea cols="30" rows="10" placeholder="Descripcion"></textarea>
+                          <button class="btn btn-primary"></button>
+                      </div>
+                  </form>
+              </div>
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
